903321c2f01638750d4adfd4e190284288850f42
jaay_kid <justincurrydev@gmail.com>
2020-07-29 15:11:46 -0700

    gametimer bug

--- src/components/GameTimer/GameTimer.jsx
+++ src/components/GameTimer/GameTimer.jsx
@@ -10,6 +10,7 @@ function formatTime(seconds) {
 }
 class GameTimer extends Component {
   handleTick = () => {
+    console.log('running');
     if (this.props.perfect === 4) {
       clearInterval(this.timerId);
     }
